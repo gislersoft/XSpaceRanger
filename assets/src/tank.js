01e86e824861ce3eada83f512b8c08aa300ec4b9
gislersoft <gislersoft@hotmail.com>
2022-12-06 18:51:10 -0500

    Solucionando bugs del refactor

--- assets/src/tank.js
+++ assets/src/tank.js
@@ -1,14 +1,16 @@
 class Tank {
 
-    constructor(scene) {
+    constructor(scene, nave, collisionCallback) {
         this.scene = scene;
-        this.speedtank;
+        this.nave = nave;
+        this.collisionCallback = collisionCallback;
+        this.speedtank = undefined;
         this.speed = 3;
         this.speedtankY = -50;
-        this.timerCreateTank;
+        this.timerCreateTank = undefined;
         this.timerCreateTankInitiated = false;
-        this.particlesTank;
-        this.timerTankParticles;
+        this.particlesTank = undefined;
+        this.timerTankParticles = undefined;
     }
 
     updateTank() {
@@ -21,14 +23,16 @@ class Tank {
         }
     }
 
-    tankCollision(callback) {
+    tankCollision() {
         this.speedtank.destroy();
         this.particlesTank.visible = true;
         this.timerTankParticles = this.scene.time.delayedCall(800, ()=>{
             this.particlesTank.visible = false;
         }, [],  this.scene);
         this.initCreateTankTimer();
-        callback();
+        if (this.collisionCallback) {
+            this.collisionCallback();
+        }
     }
 
     // Crea el sistema de particulas cuando colisiona con el tanque
@@ -47,15 +51,15 @@ class Tank {
     }
 
     // Crea el tanque y sus particulas
-    createTank(nave, collideCallback) {
+    createTank() {
         this.speedtankY = -50;
-        this.speedtank =  this.scene.physics.add.image(Phaser.Math.Between(0, 799), 60, 'speedtank');
+        this.speedtank = this.scene.physics.add.image(Phaser.Math.Between(0, 799), 60, 'speedtank');
         this.speedtank.setScale(0.1);
 
-        this.particlesTank = this.createTankParticleSystem(nave, this.scene.add.particles('violet'));
+        this.particlesTank = this.createTankParticleSystem(this.nave, this.scene.add.particles('violet'));
 
-        this.scene.physics.add.collider(nave,  this.speedtank, ()=>{
-            this.tankCollision(collideCallback);
+        this.scene.physics.add.collider(this.nave,  this.speedtank, ()=>{
+            this.tankCollision(this.nave, this.collisionCallback);
         });
 
         this.timerCreateTankInitiated = false;
@@ -64,7 +68,9 @@ class Tank {
     initCreateTankTimer() {
         if (!this.timerCreateTankInitiated) {
             this.timerCreateTankInitiated = true;
-            this.timerCreateTank = this.scene.time.delayedCall(Phaser.Math.Between(4000, 30000),  this.createTank, [], this.scene);
+            this.timerCreateTank = this.scene.time.delayedCall(Phaser.Math.Between(4000, 30000),  ()=>{
+                this.createTank();
+            }, [], this.scene);
         }
     }
 }
